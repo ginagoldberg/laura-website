32d5459ff59263189a5fd4be928d9a697e100024
Gina Goldberg <gina.goldberg@hotmail.com>
2016-08-30 13:19:50 +0200

    DRY code, add before-action

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,6 @@
 class ArtworksController < ApplicationController
+before_action :set_artwork, only: [:edit, :update, :destroy]
+
   def new
     @artwork = Artwork.new
   end
@@ -14,11 +16,9 @@ class ArtworksController < ApplicationController
   end
 
   def edit
-    @artwork = Artwork.find(params[:id])
   end
 
   def update
-    @artwork = Artwork.find(params[:id])
     @artwork.update(art_params)
       if @artwork.save
       redirect_to artwork_path(@artwork),  :alert => "#{@artwork.name} is updated"
@@ -28,12 +28,15 @@ class ArtworksController < ApplicationController
   end
 
   def destroy
-    @artwork = Artwork.find(params[:id])
     @artwork.destroy
     redirect_to root_path, :alert => "#{@artwork.name} is deleted"
   end
 
   private
+
+  def set_artwork
+    @artwork = Artwork.find(params[:id])
+  end
 
   def art_params
     params.require(:artwork).permit(:name, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get '/:id', to: 'pages#show', as: 'artwork'
+  get 'art/:id', to: 'pages#show', as: 'artwork'
 
   get '/contact', to: 'pages#contact'
 
   scope '/admin' do
-    resources :artworks, except: [:show, :index], path: 'art'
+    resources :artworks, except: [:show, :index], path: ''
   end
 
 end
