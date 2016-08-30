0c68b8f01849b2524953c378f05cd70dae7ddf05
Gina Goldberg <gina.goldberg@hotmail.com>
2016-08-30 16:54:55 +0200

    add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'pages#home'
 
   get 'art/:id', to: 'pages#show', as: 'artwork'
 
   get '/contact', to: 'pages#contact'
 
-  scope '/admin' do
-    resources :artworks, except: [:show, :index], path: ''
-  end
+  # scope '/admin' do
+  #   resources :artworks, except: [:show, :index], path: ''
+  # end
 
 end
