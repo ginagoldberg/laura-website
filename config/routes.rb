8768b4be35ed89ea1aa1b44585c38cf709a92caa
Gina Goldberg <gina.goldberg@hotmail.com>
2016-08-30 17:03:12 +0200

    remove artworks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'pages#home'
 
   get 'art/:id', to: 'pages#show', as: 'artwork'
 
   get '/contact', to: 'pages#contact'
 
-  # scope '/admin' do
-  #   resources :artworks, except: [:show, :index], path: ''
-  # end
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
 end
